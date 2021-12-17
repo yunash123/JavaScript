32c9bd40d401b9a4a61f1b717facec2497701cd0
Yunash 469 <33439324+yunash123@users.noreply.github.com>
2021-12-17 12:24:47 -0300

    salvar

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,8 +1,33 @@
-var n1 =document.getElementById('txtinicio')
-var n2 = document.getElementById('txtfim')
-var n3 = document.getElementById('txtpasso')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-// var nEntrada = Number(n1.value)
-// var nFinal = Number(n2.value)
-// var nPasso = Number(n3.value)
-
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = 'Impossovel contar!'
+       // window.alert('[ERRO] Faltam dados !')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window.alert('Passo invalido! Considerando PASSO 1')
+            p = 1
+        }
+    if (i <f){
+        //Contagem crecente
+        for(let c = i; c <= f ; c += p){
+            res.innerHTML += ` ${c} \u{1f449}`
+        }
+      
+        }else {
+            //Contagem regressiva
+            for(let c = i; c >=f; c -= p){
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+        }
+          res.innerHTML += `\u{1f3c1}`
+    }
+}
